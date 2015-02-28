aa600483e3aac1dfddbe78992f957a4b53d1470c
Kelvin Tay <kelvintaywl@gmail.com>
2015-03-01 00:46:05 +0900

    [master] update variables

--- examples/test.py
+++ examples/test.py
@@ -4,9 +4,9 @@ import requests
 
 
 client = TinyPng(api_key="YOUR_API_KEY")
-url = "http://docs.python-requests.org/en/latest/_static/requests-sidebar.png"
+image = requests.get("http://docs.python-requests.org/en/latest/_static/requests-sidebar.png").content
 try:
-    resp = client.shrink(data=requests.get(url).content)
+    resp = client.shrink(data=image)
     print("Compressed file can be found at: {}".format(resp.headers.get("location")))
 
 except Exception as e:
